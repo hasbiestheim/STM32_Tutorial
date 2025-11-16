8c145ef8cc8483217de7a91e04373b65658aed76
hasbiestheim <hasbiida@gmail.com>
2025-11-17 06:56:44 +0700

    modified:   main_interrupt.c

--- Timer/main_interrupt.c
+++ Timer/main_interrupt.c
@@ -1,52 +1,56 @@
-/* Interrupt Timer Example for STM32F411 BlackPill */
-/* main_polling.c
-   Bare-metal blink using TIM2 on STM32F411 (BlackPill).
-   Polls TIM2 update flag (UIF) for a ~1s delay.
+/* main.c
+   CMSIS example for STM32F411CE
+   TIM2: PSC = 15999, ARR = 999 -> update interrupt used to toggle PC13.
+   Assumes standard startup (SystemInit) provided by PlatformIO.
 */
-#define STM32F411xE
 #include "stm32f4xx.h"
-#include "stm32f411xe.h"
 
-int main(void) {
-    /* 1. Enable GPIOC clock */
+
+/* The name of the IRQ function should match the one in the vector table in the startup file */
+void TIM2_IRQHandler(void){
+    /* Check what type of event occurred */
+    if (TIM2->SR & TIM_SR_UIF)
+    {
+        /* Clear the interrupt even flag. CPU will only respond to new flags thereafter */
+        TIM2->SR &= ~(TIM_SR_UIF);
+        GPIOC->ODR ^= GPIO_ODR_OD13;
+    }
+}
+
+int main(){
+    /* Enable GPIOC Clock */
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-
-    /* 2. Configure PC13 as general purpose output (01) */
+    /* Set Port C Pin 13 as Output */
     GPIOC->MODER &= ~(0x3UL << (13 * 2));
     GPIOC->MODER |=  (0x1UL << (13 * 2));
-    GPIOC->OTYPER &= ~(1 << 13);
-    GPIOC->OSPEEDR &= ~(0x3UL << (13 * 2));
-    GPIOC->PUPDR &= ~(0x3UL << (13 * 2));
-
-    /* 3. Enable TIM2 clock (APB1) */
+    // GPIOC->OTYPER &= ~(1 << 13);
+    // GPIOC->OSPEEDR &= ~(0x3UL << (13 * 2));
+    
+    /* Enable Timer 2 peripheral */
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-
-    /* 4. Configure TIM2: PSC and ARR for 1s period */
-    TIM2->PSC = 49999;    // prescaler for 1000 Hz
-    TIM2->ARR = 999;      // auto-reload value for 1s
-    TIM2->CNT = 0;       // reset counter
-
-    /* 5. Enable update interrupt */
+    /* Default APB1 speed is 16MHz. */
+    TIM2->PSC = 16000 - 1;
+    /* Disable Timer*/
+    TIM2->CR1 = 0;
+    /* Reinitialize all registers */
+    TIM2->EGR = TIM_EGR_UG;
+    /* 1000 Counts at 1hz IRQ */
+    TIM2->ARR = 1000 - 1;
+    
+    /* Enable hardware interrupts */
     TIM2->DIER |= TIM_DIER_UIE;
-
-    /* 6. Enable NVIC for TIM2 */
-    NVIC_EnableIRQ(TIM2_IRQn);
-
-    /* 7. Enable counter */
+    TIM2->SR &= ~(1<<0);
+    /* Start Timer */
     TIM2->CR1 |= TIM_CR1_CEN;
 
-    while (1) {
-        // Main loop does nothing; LED toggled in interrupt
-        // Add your code here
-    }
+    /* These are CMSIS calls to enable interrupts */
+    NVIC_SetPriority(TIM2_IRQn, 2);
+    NVIC_EnableIRQ(TIM2_IRQn);
+    // NVIC->IP[TIM2_IRQn] =  (2 << 4); // Set priority to 2
+    // NVIC->ISER[TIM2_IRQn >> 5] |= (1 << (TIM2_IRQn % 32)); // Enable interrupt
 
-    return 0;
-}
-
-void TIM2_IRQHandler(void) {
-    if (TIM2->SR & TIM_SR_UIF) {
-        TIM2->SR &= ~TIM_SR_UIF;  // clear UIF
-        GPIOC->ODR ^= (1 << 13);  // toggle LED PORTC13
+    while (1){
+        /* Do nothing here */
+                /* Toggle LED */
     }
 }
-
